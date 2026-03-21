aea040ff88359b819c040418406b8c5318e11ced
terminalgabut <trasipahit02@gmail.com>
2026-03-21 15:46:05 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 import Layout from '../components/layout.js';
 import { router } from './router.js';
+import { Logger } from './services/debug.js'; // Import logger
 
 const app = Vue.createApp({
-    components: {
-        'layout-wrapper': Layout
-    },
+    components: { 'layout-wrapper': Layout },
     template: `<layout-wrapper />`
 });
 
+// --- GLOBAL ERROR HANDLER ---
+app.config.errorHandler = (err, vm, info) => {
+    Logger.error('Vue Component', { err, info });
+};
+
+// Pasang Logger ke global agar bisa dipanggil via this.$log di komponen
+app.config.globalProperties.$log = Logger;
+
 app.use(router);
 app.mount('#app');
+
+Logger.info('App Initialized Successfully');
