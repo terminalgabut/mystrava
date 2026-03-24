9aec1e84b783484013edb7a29f71c266e30423c1
terminalgabut <trasipahit02@gmail.com>
2026-03-24 20:10:14 +0700

    Update sidebarView.js

--- components/sidebarView.js
+++ components/sidebarView.js
@@ -8,7 +8,7 @@ export default {
                     <div class="w-8 h-8 bg-blue-600 rounded-lg flex items-center justify-center shadow-lg shadow-blue-200">
                         <i data-lucide="zap" class="w-5 h-5 text-white"></i>
                     </div>
-                    <span class="text-xl font-black italic text-slate-900 tracking-tighter">DS.PRO</span>
+                    <span class="text-xl font-black italic text-slate-900 tracking-tighter">FRD.PRO</span>
                 </div>
             </div>
 
